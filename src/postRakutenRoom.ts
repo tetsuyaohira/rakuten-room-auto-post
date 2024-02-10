a3d3bbe99f03018bf6e9ead142c10c9896558511
tetsuyaohira <tetsuya.ohira@icloud.com>
2024-02-10 18:00:01 +0900

    調整

--- src/postRakutenRoom.ts
+++ src/postRakutenRoom.ts
@@ -3,13 +3,15 @@ import scrapeWebsite from "./scrapeWebsite";
 async function postRakutenRoom(elements: any) {
   for (const element of elements) {
     try {
-      const { catchcopy, itemName, itemCaption, itemCode } = element.Item;
+      const { catchcopy, itemName, itemCode } = element.Item;
 
       console.log("-----------------------------------------------");
       const url = `https://room.rakuten.co.jp/mix?itemcode=${itemCode}&scid=we_room_upc60`;
       console.log(url);
 
-      await scrapeWebsite(url, catchcopy, itemName, itemCaption);
+      await scrapeWebsite(url, catchcopy, itemName);
+
+      await new Promise((resolve) => setTimeout(resolve, 90000)); // 1分３０秒待つ
     } catch (error) {
       console.error("Error:", error);
     }
