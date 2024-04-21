a163fe81407efce1e968bfdc2607ae7cea40dd8c
tetsuyaohira <tetsuya.ohira@icloud.com>
2024-04-21 12:56:03 +0900

    パラメタ指定を修正

--- src/parseCommandLineArgs.ts
+++ src/parseCommandLineArgs.ts
@@ -2,13 +2,13 @@ const parseCommandLineArgs = (args: string[]) => {
   const options: { genre?: string; keyword?: string } = {};
 
   for (let i = 0; i < args.length; i++) {
-    if (args[i] === "-genre") {
+    if (args[i] === "genre") {
       if (options.genre) {
         throw new Error("エラー: ジャンルは既に指定されています。");
       }
       options.genre = args[i + 1];
       i++;
-    } else if (args[i] === "-keyword") {
+    } else if (args[i] === "keyword") {
       if (options.keyword) {
         throw new Error("エラー: キーワードは既に指定されています。");
       }
